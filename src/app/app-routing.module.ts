046b7f359a2cff2030d94113ca0b082d177f94f5
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-05-05 11:48:57 +0530

    home page 90% done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    // component: FooterComponent,
+    // component: NavigationComponent,
   },
   {
     path: 'register',
