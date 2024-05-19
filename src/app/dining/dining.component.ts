993a647fb5e95d92780663556b021d44fcb936b7
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-05-19 11:24:19 +0530

    menus added to the restaurants, but not responsive yet

--- src/app/dining/dining.component.ts
+++ src/app/dining/dining.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dining.component.css']
 })
 export class DiningComponent {
-
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
