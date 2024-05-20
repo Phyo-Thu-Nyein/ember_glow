f61679d874372adaaffc611627ca1360ae61313e
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-05-20 14:06:21 +0530

    restaurants are separated as a component from the dining component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { DiningComponent } from './dining/dining.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReceptionComponent } from './reception/reception.component';
+import { DiningRestaurantsComponent } from './dining-restaurants/dining-restaurants.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { ReceptionComponent } from './reception/reception.component';
     AboutComponent,
     ContactComponent,
     ReceptionComponent,
+    DiningRestaurantsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dining-restaurants/dining-restaurants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dining-restaurants',
+  templateUrl: './dining-restaurants.component.html',
+  styleUrls: ['./dining-restaurants.component.css']
+})
+export class DiningRestaurantsComponent {
+
+}
