7baa99d9cdd1a96eee23f0d80dc6f4561df3d7c9
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-07-07 11:35:36 +0530

    contact page updated. ts file, web3forms using angular

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MailService } from '../services/mail.service';
+import { NgForm } from '@angular/forms';
+import { env } from 'src/env/env';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +9,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  constructor(
+    private mailService: MailService
+  ) { }
+
+  private color: string = '';
+  showAlert: boolean = false;
+  alertMessage: string = '';
+  onSubmit: boolean = false;
+  contactFormValues = {
+    name: '',
+    email: '',
+    body: '',
+  };
+
+  get alertColor() {
+    return `text-${this.color}-400`;
+  }
+
+  hideAlert() {
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 3000);
+  }
+
+  async submitEmail(contactForm: NgForm) {
+    this.onSubmit = true;
+    // set formData values
+    let formData: FormData = new FormData();
+    formData.append('name', this.contactFormValues.name);
+    formData.append('email', this.contactFormValues.email);
+    formData.append('body', this.contactFormValues.body);
+    // email customization
+    formData.append('access_key', env.form_access_key);
+    formData.append('subject', 'General Inquiry From My Site');
+    formData.append('from_name', 'Contact Notification');
+
+    try {
+      // send email
+      const res = await this.mailService.sendEmail(formData);
+      if (!res.ok) {
+        throw new Error();
+      }
+      this.alertMessage = 'Email sent successfully!'
+      this.color = 'green';
+      contactForm.reset();
+    } catch (err) {
+      // handle error
+      this.alertMessage = 'Something went wrong, try again later!';
+      this.color = 'red';
+    }
+    // reset submit & hide alert
+    this.onSubmit = false;
+    this.showAlert = true;
+    this.hideAlert();
+  }
 
 }
---
+++ src/env/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+    production: false,
+    form_access_key: 'a7eb4194-6103-4c3f-b311-f8480c3e19a2'
+}
