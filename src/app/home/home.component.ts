0eb82cc047b33dffbdf2f6297f0a914f95da8ad0
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2024-08-01 16:47:02 +0530

    loading transition is updated in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
-  constructor (private router:Router) {}
+  constructor (private router:Router, private loadingService: LoadingService) {}
 
   goToRooms() {
-    this.router.navigateByUrl('rooms');
+    this.loadingService.showLoading(); // loading b4 navigate
+    setTimeout(() => {
+      this.router.navigateByUrl('rooms');
+    }, 460);
   }
 }
